d571fbfd4e800539ec5d6bb743ec6970d55c9e4a
Locação <locacao@C02DX4N2MD6P.local>
2022-07-20 14:41:40 -0700

    setup do tema do jogo da memoria

---
+++ tema-jogo-da-memoria/Pods/Target Support Files/Pods-Jogo da Memoria-Jogo da MemoriaUITests/Pods-Jogo da Memoria-Jogo da MemoriaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jogo_da_Memoria_Jogo_da_MemoriaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Jogo_da_Memoria_Jogo_da_MemoriaUITests
+@end
---
+++ tema-jogo-da-memoria/Pods/Target Support Files/Pods-Jogo da Memoria/Pods-Jogo da Memoria-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jogo_da_Memoria : NSObject
+@end
+@implementation PodsDummy_Pods_Jogo_da_Memoria
+@end
